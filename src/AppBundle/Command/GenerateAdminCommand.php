41527a2c18762d6fee40160fad89d81f20972abd
Your Name <you@example.com>
2017-05-19 04:14:07 +0200

    add changes

---
+++ src/AppBundle/Command/GenerateAdminCommand.php
@@ -0,0 +1,38 @@
+<?php
+namespace AppBundle\Command;
+
+use Sensio\Bundle\GeneratorBundle\Command\GenerateDoctrineCrudCommand;
+use Sensio\Bundle\GeneratorBundle\Generator\DoctrineCrudGenerator;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+/**
+* @class GenerateDoctrineCrudCommand
+* This Classs Override GenerateDoctrineCrudCommand class
+* 
+*/
+class GenerateAdminCommand extends GenerateDoctrineCrudCommand
+{
+    protected function configure()
+    {
+         
+        dump('You should know...');exit;
+        parent::configure();
+        $this->setName('app:generate:crud');
+        $this->setDescription('Coucou, je suis la surcharge');
+    }
+
+    protected function getGenerator(BundleInterface $bundle = null)
+    {
+        if (null === $this->generator)
+        {
+            $this->generator = new DoctrineCrudGenerator($this->getContainer()->get('filesystem'), __DIR__.'/../Resources/skeleton/crud');
+        }
+
+        return $this->generator;
+    }
+
+}
